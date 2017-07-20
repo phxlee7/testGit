d09443b59fe618014ece6563ffa4ed82bedbabd7
lance7 <phxlee7@foxmail.com>
2017-07-20 15:58:12 +0800

    1
    test

---
+++ src/main/java/com/ruijie/DroolsResult.java
@@ -0,0 +1,17 @@
+package com.ruijie;
+
+/**
+ * Created by liwenqi on 2017/7/14.
+ */
+public class DroolsResult {
+
+    private String result;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
---
+++ src/main/java/com/ruijie/Obj.java
@@ -0,0 +1,16 @@
+package com.ruijie;
+
+/**
+ * Created by liwenqi on 2017/7/12.
+ */
+public class Obj {
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
